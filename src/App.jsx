ffd1b015af23f8c10d8ffe9012bdc8846ffbe34f
Sumit Kumar. <ncr2099@gmail.com>
2025-10-03 18:37:45 +0530

    Add country card UI and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountryData = async () => {
+    try {
+      const response = await axios.get(
+        "https://xcountries-backend.labs.crio.do/all"
+      );
+      setCountries(response.data);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountryData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {countries.map((country) => (
+        <div key={country.abbr} className="country-card">
+          <img src={country.flag} alt={country.name} width="100%" />
+          <p>{country.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default App
